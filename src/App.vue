f288c5a61e8b1892d0bdacf26e53852f8c946fbf
Kun <freestyle5533@hotmail.com.tw>
2019-09-05 13:15:18 +0800

    VueShopDemo init &  get API data

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  created() {
+    console.log(process.env.APIPATH, process.env.CUSTOMPATH);
+    const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
+    this.$http.get(api).then(response => {
+      console.log(response);
+    });
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
